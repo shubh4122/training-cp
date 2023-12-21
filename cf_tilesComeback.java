4c56f923be1b746f66949a0ba3135b4ccf084625
Shubham Pandey <shubhampandeylearning@gmail.com>
2023-12-21 23:38:00 +0530

    Added ques link

--- cf_tilesComeback.java
+++ cf_tilesComeback.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
+//https://codeforces.com/problemset/problem/1851/C
 public class cf_tilesComeback {
     public static void main(String[] args) throws IOException {
 
